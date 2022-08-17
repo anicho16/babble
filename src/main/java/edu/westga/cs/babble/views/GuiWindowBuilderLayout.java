08b2ab3c50b7bff35a6b89deaa0024685cd7cbb9
Amber Nicholas <anicho16@my.westga.edu>
2022-08-17 09:41:01 -0500

    Completed inactive GUI. Pending action events

--- src/main/java/edu/westga/cs/babble/views/GuiWindowBuilderLayout.java
+++ src/main/java/edu/westga/cs/babble/views/GuiWindowBuilderLayout.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JList;
 
 public class GuiWindowBuilderLayout extends JFrame {
 
@@ -24,6 +25,7 @@ public class GuiWindowBuilderLayout extends JFrame {
 	protected JTextField textField_2;
 	protected JLabel lblNewLabel_1;
 	protected JLabel lblNewLabel_2;
+	protected JList list;
 
 	/**
 	 * Launch the application.
@@ -53,7 +55,7 @@ public class GuiWindowBuilderLayout extends JFrame {
 		this.contentPane.setLayout(null);
 		
 		this.textField = new JTextField();
-		this.textField.setBounds(39, 87, 343, 19);
+		this.textField.setBounds(39, 102, 343, 19);
 		this.contentPane.add(this.textField);
 		this.textField.setColumns(10);
 		
@@ -62,7 +64,7 @@ public class GuiWindowBuilderLayout extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		this.btnNewButton.setBounds(138, 116, 114, 21);
+		this.btnNewButton.setBounds(149, 131, 114, 21);
 		this.contentPane.add(this.btnNewButton);
 		
 		this.btnNewButton_1 = new JButton("Play Word");
@@ -70,17 +72,17 @@ public class GuiWindowBuilderLayout extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		this.btnNewButton_1.setBounds(273, 116, 109, 21);
+		this.btnNewButton_1.setBounds(273, 131, 109, 21);
 		this.contentPane.add(this.btnNewButton_1);
 		
 		this.textField_1 = new JTextField();
-		this.textField_1.setBounds(262, 163, 120, 19);
+		this.textField_1.setBounds(262, 176, 120, 19);
 		this.contentPane.add(this.textField_1);
 		this.textField_1.setColumns(10);
 		
 		this.lblNewLabel = new JLabel("Score:");
 		this.lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		this.lblNewLabel.setBounds(207, 164, 45, 18);
+		this.lblNewLabel.setBounds(218, 177, 45, 18);
 		this.contentPane.add(this.lblNewLabel);
 		
 		this.textField_2 = new JTextField();
@@ -88,14 +90,18 @@ public class GuiWindowBuilderLayout extends JFrame {
 		this.contentPane.add(this.textField_2);
 		this.textField_2.setColumns(10);
 		
-		this.lblNewLabel_1 = new JLabel("Your Word:");
+		this.lblNewLabel_1 = new JLabel("Your Word");
 		this.lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		this.lblNewLabel_1.setBounds(39, 61, 85, 19);
+		this.lblNewLabel_1.setBounds(39, 73, 85, 19);
 		this.contentPane.add(this.lblNewLabel_1);
 		
 		this.lblNewLabel_2 = new JLabel("Tiles");
 		this.lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		this.lblNewLabel_2.setBounds(39, 10, 45, 13);
 		this.contentPane.add(this.lblNewLabel_2);
+		
+		this.list = new JList();
+		this.list.setBounds(39, 33, 343, 30);
+		this.contentPane.add(this.list);
 	}
 }
